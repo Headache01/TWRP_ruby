03398fff724d6bb6f1143c49a7657d652be54470
Headache80 <ismailduyan80@gmail.com>
2024-04-18 22:51:50 +0300

    ruby: Rebase & add missing files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,13 +13,15 @@ ALLOW_MISSING_DEPENDENCIES := true
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
-    vendor \
     system \
-    boot \
-    vbmeta_system \
+    system \
+    vendor \
+    vendor \
     product \
+    product \
+    boot \
     vbmeta_vendor \
-    system_ext
+    vbmeta_system
 BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture
@@ -82,7 +84,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
 BOARD_SUPER_PARTITION_GROUPS := xiaomi_dynamic_partitions
-BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system vendor vendor system_ext system_ext product product mi_ext mi_ext
+BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system vendor vendor product product
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
 
 # Platform
